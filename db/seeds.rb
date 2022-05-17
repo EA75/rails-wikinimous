e901619dc359b27d68fb92d6e44c21e8f9d6abe3
Emmanuelle Amouroux <amouroux.emmanuelle@orange.fr>
2022-05-17 15:11:11 +0200

    avant mise en forme

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# TODO: Write a seed
+# Write a seed that populates your database with 5 users who each have between 5 and 10 posts.
+
+require "faker"
+
+10.times do
+  article = Article.new(title: Faker::Sports::Football.team, content: Faker::Sports::Football.competition)
+  article.save
+end
